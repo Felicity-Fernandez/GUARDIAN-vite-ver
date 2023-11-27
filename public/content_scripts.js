f9d90e5f892953b2440ecffb4aea3449d8772117
Jasmine-Abucejo <abucejojasmine@gmail.com>
2023-11-27 16:20:22 +0800

    trial

--- public/content_scripts.js
+++ public/content_scripts.js
@@ -1,5 +1,7 @@
-// const nlp = require("compromise");
+// import nlp from "compromise";
+
 console.log("running");
+let tweets = null;
 chrome.runtime.sendMessage({ action: "getFilterSites" }, (toFilterSites) => {
   const currentUrl = window.location.href;
   if (toFilterSites.some((site) => currentUrl.includes(site))) {
@@ -11,14 +13,25 @@ chrome.runtime.sendMessage({ action: "getFilterSites" }, (toFilterSites) => {
       document.readyState === "complete" ||
       document.readyState === "interactive"
     ) {
-      console.log(document.querySelector("main"));
-      processTweets();
+      const interval = setInterval(() => {
+        if (
+          document.querySelector('[data-testid = "cellInnerDiv"]')
+            ?.parentElement
+        ) {
+          console.log("ready...");
+          clearInterval(interval);
+          processTweets();
+        }
+      }, 1000);
     }
   }
 });
 function processTweets() {
-  const targetNode =
-    document.querySelector(".css-1dbjc4n").children[0].children[0];
+  console.log("started");
+  let targetNode = document.querySelector(
+    '[data-testid = "cellInnerDiv"]'
+  ).parentElement;
+
   console.log(targetNode);
 
   // Options for the observer (which mutations to observe)
@@ -29,16 +42,21 @@ function processTweets() {
     for (const mutation of mutationList) {
       if (mutation.type === "childList") {
         console.log("A child node has been added or removed.");
-        const tweets = document.querySelector("main").children;
+        tweets = document.querySelectorAll('[data-testid = "cellInnerDiv"]');
+
         // console.log(tweets);
-        [...tweets].forEach((tweet) => {
-          // const doc = nlp(tweet);
-          // const filtered = doc.match("(swear|badword|offensive|arse)").replace("****");
-          // tweet.innerText.replace(filtered.out("text"));
-          console.log(tweet.innerText);
-        });
       }
     }
+    console.log(tweets);
+    // if (tweets) {
+    //   [...tweets].forEach((tweet) => {
+    //     console.log(tweets);
+    //     const doc = nlp(tweet);
+    //     const filtered = doc.match("(swear|badword|offensive|arse)").replace("****");
+    //     tweet.innerText.replace(filtered.out("text"));
+    //     console.log(tweet.innerText);
+    //   });
+    // }
   };
 
   // Create an observer instance linked to the callback function
@@ -57,3 +75,4 @@ function processTweets() {
   //   console.log(tweet.innerText);
   // });
 }
+console.log(tweets);
