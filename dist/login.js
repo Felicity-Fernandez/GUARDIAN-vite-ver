329889c0ef35b47f6d0be52fbf645714b54ed51a
Jasmine-Abucejo <abucejojasmine@gmail.com>
2024-03-08 15:04:30 +0800

    login page pt2

--- dist/login.js
+++ dist/login.js
@@ -1,47 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
   var resetBtn = document.getElementById("resetBtn");
   var loginBtn = document.getElementById("login");
-  var uname = document.getElementById("uname");
   var pass = document.getElementById("psw");
-  if (uname.value === "" || uname.value === null) {
+  if (pass.value === "" || pass.value === null) {
     loginBtn.disabled = true;
+    resetBtn.disabled = true;
     console.log("disabled");
   }
-  uname.addEventListener("input", function () {
-    if (
-      uname.value.trim() === "" ||
-      uname.placeholder === "Enter New Username"
-    ) {
+  pass.addEventListener("input", function () {
+    if (pass.value.trim() === "" || pass.placeholder === "Enter New Username") {
       // If empty, disable the button
       loginBtn.disabled = true;
+      resetBtn.disabled = true;
     } else {
       // If not empty, enable the button
       loginBtn.disabled = false;
+      resetBtn.disabled = false;
     }
   });
   resetBtn.addEventListener("click", function () {
-    uname.placeholder = "Enter New Username";
-    pass.placeholder = "Enter New Password";
-    resetBtn.textContent = "Save";
-    resetBtn.addEventListener("click", function () {
-      console.log(uname);
-      //   var uname = document.getElementById("uname");
-      //   var pass = document.getElementById("psw");
-      //   chrome.storage.sync.set(
-      //     { uname: uname.value, pass: pass.value },
-      //     function () {
-      //       console.log(
-      //         "Username and Password updated:",
-      //         uname.value,
-      //         pass.value
-      //       );
-      //     }
-      //   );
-      //   resetBtn.textContent = "Reset";
-      //   uname.value = "";
-      //   pass.value = "";
-      //   uname.placeholder = "Username";
-      //   pass.placeholder = "Password";
-    });
+    if (resetBtn.textContent === "Set New") {
+      pass.placeholder = "Enter New Password";
+      pass.value = "";
+      resetBtn.textContent = "Save";
+      loginBtn.disabled = true;
+    } else {
+      pass.placeholder = "Enter Password";
+
+      pass.value = "";
+      resetBtn.textContent = "Set New";
+    }
+  });
+  loginBtn.addEventListener("click", function () {
+    window.location.href = "index.html";
   });
 });
